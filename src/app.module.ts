139695c3f7c5af25f10d2235fa05f20eeafc66f1
Thomas Smyth <thomas.smyth3141@gmail.com>
2022-01-31 13:36:10 +0000

    Add database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as fs from 'fs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      ssl: {
+        ca: fs.readFileSync(process.env.SSL_CA_CERTIFICATES),
+      },
+
+      autoLoadEntities: true,
+
+      // Only enable this option if your application is in development,
+      // otherwise use TypeORM migrations to sync entity schemas:
+      // https://typeorm.io/#/migrations
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
