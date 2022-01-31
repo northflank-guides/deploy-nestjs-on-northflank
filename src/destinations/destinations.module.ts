d27b7e0128dd3739054f365c306ea70c2d692935
Thomas Smyth <thomas.smyth3141@gmail.com>
2022-01-31 13:11:20 +0000

    Implement destinations endpoints

--- src/destinations/destinations.module.ts
+++ src/destinations/destinations.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { DestinationsService } from './destinations.service';
 import { DestinationsController } from './destinations.controller';
+import { Destination } from './entities/destination.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Destination])],
   controllers: [DestinationsController],
   providers: [DestinationsService],
 })
